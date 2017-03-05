f3d2757c83803372d50f125177e622c5cf75b837
nagyszem22 <takacsmarton22@gmail.com>
2017-03-05 12:45:52 +0100

    changed database config

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'ec2-52-56-169-32.eu-west-2.compute.amazonaws.com'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'experience_main'),
             'username' => env('DB_USERNAME', 'root'),
